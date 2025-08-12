9197e118808848f99ff474694f2e9d7bbe166d92
bravolambo <bravomzogo@gmail.com>
2025-08-13 02:16:54 +0300

    soon

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Your Vite frontend URL
     "http://127.0.0.1:5173",
+    "cinetro.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
